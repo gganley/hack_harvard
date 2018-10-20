eb059141cab8d27d07d7d2c01e4ffe4987b2244f
gganley <gganley@student.bridgew.edu>
2018-10-20 07:42:27 -0400

    Change framework to gin, change signiture of maps, implement addentry

--- main.go
+++ main.go
@@ -2,76 +2,93 @@ package main
 
 import (
 	"log"
-	// "fmt"
-	// "io"
-	"net/http"
 	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
-type usernamePw struct {
-	uname string
-	pw    string
-}
-
 type passwordEntry struct {
-	entryUsername string
-	password      string
-	domain        string
+	username string
+	password string
+	domain   string
 }
 
 var (
-	userExistsDB map[string]string
+	userExistsDB = make(map[string]string)
 
-	userDataDB map[usernamePw][]passwordEntry
+	userDataDB = make(map[string][]passwordEntry)
 )
 
 func main() {
-	http.HandleFunc("/getdata", GetData)
-	http.HandleFunc("/createuser", CreateUser)
-	err := http.ListenAndServeTLS(":443", "ssl/dev.gganley.com.crt", "ssl/dev.gganley.com.key", nil)
+	r := gin.Default()
+	r.POST("/getdata", GetData)
+	r.POST("/createuser", CreateUser)
+	r.POST("/add", AddEntry)
+	err := r.RunTLS(":443", "ssl/dev.gganley.com.crt", "ssl/dev.gganley.com.key")
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func CreateUser(writer http.ResponseWriter, req *http.Request) {
+func CreateUser(c *gin.Context) {
 	// inserts uname and pw into `userExistsDB`
 
 	// {uname: "gganley", pw: "hellokitty"}
-	uname := req.PostForm.Get("uname")
-	pw := req.PostForm.Get("pw")
+	// uname := req.PostForm.Get("uname")
+
+	uname := c.PostForm("uname")
+	pw := c.PostForm("pw")
 
 	_, ok := userExistsDB[uname]
 
 	if ok {
-		_, e := writer.Write([]byte("Username alread exists"))
+		_, e := c.Writer.Write([]byte("Username alread exists"))
 		if e != nil {
 			log.Fatal(e)
 		}
 	} else {
 		// TODO: Actually implement this
 		userExistsDB[uname] = pw
-		userDataDB[usernamePw{uname, pw}] = []passwordEntry{}
+		userDataDB[uname] = []passwordEntry{}
 	}
 }
 
-func GetData(writer http.ResponseWriter, req *http.Request) {
+func AddEntry(c *gin.Context) {
+	uname, _, ok, matches := auth(c)
+	entryUsername := c.PostForm("entry_username")
+	entryPassword := c.PostForm("entry_password")
+	entryDomain := c.PostForm("entry_domain")
+	if ok && matches {
+		userDataDB[uname] = append(userDataDB[uname], passwordEntry{entryUsername, entryPassword, entryDomain})
+	}
+}
+
+func GetData(c *gin.Context) {
 	// Authenticate the user and then give their personal data
-	uname := req.PostForm.Get("uname")
-	pw := req.PostForm.Get("pw")
+	uname, _, ok, matches := auth(c)
 
-	userObject := usernamePw{uname, pw}
-	val, ok := userExistsDB[uname]
-
-	if ok && val == pw {
-		userEntries := userDataDB[userObject]
+	if ok && matches {
+		userEntries := userDataDB[uname]
 		for _, v := range userEntries {
 
-			_, err := writer.Write([]byte(strings.Join([]string{v.domain, v.entryUsername, v.password}, ",")))
+			_, err := c.Writer.Write([]byte(strings.Join([]string{v.domain, v.username, v.password}, ",")))
 
 			if err != nil {
 				log.Panic(err)
 			}
 		}
 	}
+}
+
+// Returns the uname and pw, ok means that the entry exists and matches means that the pw is valid
+func auth(c *gin.Context) (uname string, pw string, ok bool, matches bool) {
+	uname = c.PostForm("uname")
+	pw = c.PostForm("pw")
+	val, ok := userExistsDB[uname]
+	if val == pw {
+		matches = true
+	} else {
+		matches = false
+	}
+	return
 }
